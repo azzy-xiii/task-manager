4fc5dd1377241796826b4411e976e4201f56d1e9
Gri <i.grigorov@mail.bg>
2013-10-27 11:33:57 +0200

    second with tests

--- test/javascripts/integration/ember_tests.js
+++ test/javascripts/integration/ember_tests.js
@@ -9,3 +9,23 @@ test("root view", function() {
     return ok(exists("*"), "Found Some HTML");
   });
 });
+
+test("show tasks", function() {
+  expect(2);
+  visit("/").then(function(){
+    tasks = TaskManager.TaskModel.FIXTURES;
+    var i;
+    for(i = 0; i < 2; i++) {
+      ok(exists(tasks[i]), 'Task is shown');
+    }
+  })
+});
+
+test("sort tasks", function(){
+  visit('/').then(function(){
+    return click("strong:contains('Description')");
+  }).then(function(){
+      descritpion = $.trim(find('tbody tr:first td:first').text());
+      equal(descritpion, '2013-10-26 00:00:00');
+  })
+});
--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -8,7 +8,38 @@ document.write('<style>#ember-testing-container { position: absolute; background
 TaskManager.rootElement = '#ember-testing';
 TaskManager.setupForTesting();
 TaskManager.injectTestHelpers();
+TaskManager.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 (typeof exports !== "undefined" && exports !== null ? exports : this).exists = function(selector) {
   return !!find(selector).length;
 };
+
+TaskManager.TaskModel.FIXTURES = [
+  {
+    id: 1,
+    description: 'Testing first',
+    state: 'opened',
+    created_at: '2013-10-26 00:00:00',
+    owner: 1,
+    performer: 2
+  },
+  {
+    id: 2,
+    description: 'Testing second',
+    state: 'opened',
+    created_at: '2013-10-25 00:00:00',
+    owner: 2,
+    performer: 1
+  }
+];
+
+TaskManager.UserModel.FIXTURES = [
+  {
+    id: 1,
+    email: 'fisrt@mail.bg'
+  },
+  {
+    id: 2,
+    email: 'second@mail.bg'
+  }
+];
